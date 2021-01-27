23a27dd3c0ddb9e075c13e078842afba477c657b
Eduardo Pereira Da Silva Neto <edopneto@MacBook-Air-de-Eduardo.local>
2021-01-27 00:18:25 -0300

    Add checks on return files

--- cloud_check_file/files/serializers.py
+++ cloud_check_file/files/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from files.models import Upload, File
+from files.models import Upload, File, Check
 
 from cloud_check_file.core.serializers import BaseSerializer
-from pprint import pprint
+from cloud_check_file.core.models import as_json
 
 
 class UploadSerializer(BaseSerializer):
@@ -19,12 +19,16 @@ class FileSerializer(BaseSerializer):
         fields = '__all__'
 
     upload_object = serializers.SerializerMethodField()
+    checks = serializers.SerializerMethodField()
 
     def get_upload_object(self, obj):
         return UploadSerializer(obj.upload).data
 
+    def get_checks(self, obj):
+        checks = Check.objects.filter(file_id=obj.id).all()
+        return as_json(checks)
 
 class CheckSerializer(BaseSerializer):
     class Meta:
-        model = File
+        model = Check
         fields = '__all__'
